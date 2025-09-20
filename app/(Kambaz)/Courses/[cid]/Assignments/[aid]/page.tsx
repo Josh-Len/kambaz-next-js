bc3ac471dcb97838d71aa45a9e017f5cd03fe7fc
Josh-Len <len.j@northeastern.edu>
2025-09-20 00:55:45 -0400

    final changes to both the assignment screen and the assignment editor

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -8,18 +8,94 @@ export default function AssignmentEditor() {
                 defaultValue="The assignment is available online Submit a link to the landing page of"
             />
             <br />
-            <table>
-                <tbody>
-                <tr>
-                    <td align="right" valign="top">
-                        <label htmlFor="wd-points">Points</label>
-                    </td>
-                    <td>
-                        <input id="wd-points" defaultValue={100} />
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
+            <div id="wd-assignments-editor">
+                <label htmlFor="wd-name">Assignment Name</label>
+                <input id="wd-name" defaultValue="A1 - ENV + HTML" />
+                <br />
+                <br />
+
+                <textarea
+                    id="wd-description"
+                    defaultValue="The assignment is available online Submit a link to the landing page of"
+                />
+                <br />
+
+                <div style={{ marginTop: "12px", marginBottom: "12px" }}>
+                    <label htmlFor="wd-points">Points</label>
+                    <input id="wd-points" defaultValue={100} />
+                </div>
+
+                <div style={{ marginTop: "12px", marginBottom: "12px" }}>
+                    <label htmlFor="wd-assignment-groups">Assignment Group</label>
+                    <select id="wd-assignment-groups">
+                        <option>ASSIGNMENTS</option>
+                    </select>
+                </div>
+
+                <div style={{ marginTop: "12px", marginBottom: "12px" }}>
+                    <label htmlFor="wd-display-grade">Display Grade As</label>
+                    <select id="wd-display-grade">
+                        <option>Percentage</option>
+                    </select>
+                </div>
+
+                <div style={{ marginTop: "12px", marginBottom: "12px" }}>
+                    <label htmlFor="wd-submission-type">Submission Type</label>
+                    <select id="wd-submission-type">
+                        <option>Online</option>
+                    </select>
+                </div>
+
+                <div style={{ marginTop: "12px", marginBottom: "12px" }}>
+                    <label>Online Entry Options:</label><br />
+
+                    <input type="checkbox" name="entry-option" id="wd-entryop-txt" />
+                    <label htmlFor="wd-entryop-txt">Text Entry</label><br />
+
+                    <input type="checkbox" name="entry-option" id="wd-entryop-url" />
+                    <label htmlFor="wd-entryop-url">Website URL</label><br />
+
+                    <input type="checkbox" name="entry-option" id="wd-entryop-rec" />
+                    <label htmlFor="wd-entryop-rec">Media Recordings</label><br />
+
+                    <input type="checkbox" name="entry-option" id="wd-entryop-ann" />
+                    <label htmlFor="wd-entryop-ann">Student Annotations</label><br />
+
+                    <input type="checkbox" name="entry-option" id="wd-entryop-file" />
+                    <label htmlFor="wd-entryop-file">File Uploads</label><br />
+                </div>
+
+                <div style={{ marginTop: "12px", marginBottom: "12px" }}>
+                    <label htmlFor="wd-text-assign-to">Assign to</label><br />
+                    <input type="text" defaultValue={'Everyone'} id="wd-text-assign-to" /> <br />
+                </div>
+
+                <div style={{ marginTop: "12px", marginBottom: "12px" }}>
+                    <label htmlFor="wd-text-fields-due"> Due</label><br />
+                    <input type="date"
+                        defaultValue="2024-05-13"
+                        id="wd-text-fields-due" /><br />
+                </div>
+
+                <div style={{ display: "flex", alignItems: "center", gap: "24px", marginTop: "12px", marginBottom: "12px" }}>
+                    <div>
+                        <label htmlFor="wd-available-from">Available From</label><br />
+                        <input type="date" id="wd-available-from" defaultValue="2024-05-06" />
+                    </div>
+                    <div>
+                        <label htmlFor="wd-available-until">Until</label><br />
+                        <input type="date" id="wd-available-until" defaultValue="2024-05-20" />
+                    </div>
+                </div>
+
+                <hr />
+
+                <div style={{ display: "flex", justifyContent: "flex-start", gap: "8px" }}>
+                    <button>Save</button>
+                    <button>Cancel</button>
+                </div>
+
+            </div>
+        </div >
     );
 }
--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -3,7 +3,7 @@ export default function Assignments() {
   return (
     <div id="wd-assignments">
       <input placeholder="Search for Assignments"
-             id="wd-search-assignment" />
+        id="wd-search-assignment" />
       <button id="wd-add-assignment-group">+ Group</button>
       <button id="wd-add-assignment">+ Assignment</button>
       <h3 id="wd-assignments-title">
@@ -11,22 +11,39 @@ export default function Assignments() {
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
           <Link href="/Courses/1234/Assignments/123"
-             className="wd-assignment-link" >
+            className="wd-assignment-link" >
             A1 - ENV + HTML
-          </Link> </li>
-            Multiple Modules | Not Availible till May 6 12:00 AM |
+          </Link> <div className="wd-assignment-meta">
+            Multiple Modules | <b>Not Available until</b> May 6 12:00 AM |
+          </div>
+          <div className="wd-assignment-due">
+            <b>Due</b> May 13 at 11:59 PM | 100 Pts
+          </div>
+        </li>
         <li className="wd-assignment-list-item">
           <Link href="/Courses/1234/Assignments/223"
-             className="wd-assignment-link" >
+            className="wd-assignment-link" >
             A2 - CSS + BOOTSTRAP
-          </Link> </li>
-            Multiple Modules | Not Availible till May 13 12:00 AM |
-            <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/323"
-             className="wd-assignment-link" >
+          </Link>
+          <div className="wd-assignment-meta">
+            Multiple Modules | <b>Not Available until</b> May 13 12:00 AM |
+          </div>
+          <div className="wd-assignment-due">
+            <b>Due</b> May 20 at 11:59 PM | 100 Pts
+          </div>
+        </li>
+        <li className="wd-assignment-list-item">
+          <Link href="/courses/1234/assignments/323" className="wd-assignment-link">
             A3 - JAVASCRIPT + REACT
-          </Link> </li>
-            Multiple Modules | Not Availible till May 20 12:00 AM |
+          </Link>
+          <div className="wd-assignment-meta">
+            Multiple Modules | <b>Not Available until</b> May 20 12:00 AM |
+          </div>
+          <div className="wd-assignment-due">
+            <b>Due</b> May 27 at 11:59 PM | 100 Pts
+          </div>
+        </li>
       </ul>
     </div>
-);}
+  );
+}
